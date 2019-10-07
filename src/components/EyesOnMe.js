38b94796cca5f3b388de000ec9fef77747f9fa10
German Garcia Gonzalez <ggarciag.business@outlook.com>
2019-10-07 11:02:36 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+class EyesOnMe extends React.Component {
+
+  good = () => {
+    console.log("Good!")
+  }
+
+  notgood = () => {
+    console.log("Hey! Eyes on me!")
+  }
+
+  render(){
+    return (
+    <div>
+    <button onFocus={this.good} onBlur={this.notgood}></button>
+    </div>
+    )
+  }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,19 @@
-// Code Keypad Component Here
+import React from 'react';
+
+
+class Keypad extends React.Component {
+
+  message = () => {
+    console.log("Entering password...")
+  }
+
+  render(){
+    return (
+    <div>
+    <input type="password" onKeyUp={this.message} />
+    </div>
+    )
+  }
+}
+
+export default Keypad;
